0234da4bac7476fbb3ae27bcd8a883b120d8351d
77shah77 <zakirov.shah@yandex.ru>
2020-04-26 13:15:18 +0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$('.career-slider').slick({
+	speed: 500,
+	slidesToShow: 1,
+	autoplay: true,
+	slidesToScroll: 1,
+	autoplaySpeed: 4000,
+	infinite: true,
+	draggable: false,
+	pauseOnFocus: false,
+	pauseOnHover: true,
+	dots: true
+});
